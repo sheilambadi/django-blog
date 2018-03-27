221f3164c96ac115d03c3c8a75a767608cabddd8
sheilambadi <mbadiatieno@gmail.com>
2018-03-27 23:08:18 +0300

    add templates for home and about page

--- djangoblog/views.py
+++ djangoblog/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def homePage(request):
-    return HttpResponse('Home')
+    # return HttpResponse('Home')
+    return render(request, 'home.html')
 
 def about(request):
-    return HttpResponse('About')
+    # return HttpResponse('About')
+    return render(request, 'about.html')
