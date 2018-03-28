4345b6cdeb776acd0ac8d92a3dd0d935b7857b83
sheilambadi <mbadiatieno@gmail.com>
2018-03-29 00:18:56 +0300

    bootstrap static files added

--- djangoblog/urls.py
+++ djangoblog/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +9,6 @@ urlpatterns = [
     path('about/', views.about),
     path('', views.homePage),
 ]
+
+#check if we are in debug mode before serving static files
+urlpatterns += staticfiles_urlpatterns()
