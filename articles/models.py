464d68a4d014220f950587618664d33f42c51b24
sheilambadi <mbadiatieno@gmail.com>
2018-03-28 23:01:47 +0300

    add article models and migrated to postgres db

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    #Thumbnail and author will be added later
+
+    #to display string version of object and not the object in the interactive shell and admin section
+    def __str__(self):
+        return self.title
