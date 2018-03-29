476270f06cf6133758414452e769aebf0721ad72
sheilambadi <mbadiatieno@gmail.com>
2018-03-29 23:06:18 +0300

    display continue and date inline

--- articles/views.py
+++ articles/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def article_list(request):
-    articles = Article.objects.all().order_by('date')
+    articles = Article.objects.all().order_by('date')[::-1]
 
     #3rd param is dictionary with data to send to html
     return render(request, 'articles/article_list.html', {'articles': articles})
