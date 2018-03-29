8826497624974e010af211ab91f7b438efa012c0
sheilambadi <mbadiatieno@gmail.com>
2018-03-29 21:40:16 +0300

    slug parameter added in url

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+#name space url file
+app_name = 'articles'
+
 urlpatterns = [
-    path('', views.article_list),
+    path('', views.article_list, name='article_list'),
+    #name capturing group path/url
+    path('<slug:slug>/', views.article_detail, name='article_detail')
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Article
+from django.http import HttpResponse
 
 # Create your views here.
 def article_list(request):
@@ -7,3 +8,6 @@ def article_list(request):
 
     #3rd param is dictionary with data to send to html
     return render(request, 'articles/article_list.html', {'articles': articles})
+
+def article_detail(request, slug):
+    return HttpResponse(slug)
