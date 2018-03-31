a3ff1bc84f0ed5b4bbcbc735c87de11b253abc9b
sheilambadi <mbadiatieno@gmail.com>
2018-04-01 01:59:32 +0300

    add login and signup forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 
 def signup_view(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:article_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data = request.POST)
+        if form.is_valid():
+            return redirect('articles:article_list')
+    else:
+        form = AuthenticationForm();
+    return render(request, 'accounts/login.html', {'form':form})
